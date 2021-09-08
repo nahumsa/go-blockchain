8bda168c6c7b58b13a255a0c007e4b22188ebec4
nahumsa <nahumsaa@gmail.com>
2021-09-08 18:07:02 -0300

    add blockchain struct tests

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -1,24 +1,63 @@
 package blockchain
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	dbTestPath = "test_blocks"
+)
+
 func TestInitBlockChain(t *testing.T) {
 
-	blockchain := InitBlockchain()
-	genesisBlock := blockchain.Blocks[0]
-	assert.Equal(t, string(genesisBlock.Data), "Genesis", "Genesis data doesn't match")
+	err := os.Mkdir(dbTestPath, 0755)
+	Handle(err)
+	defer os.RemoveAll(dbTestPath)
+
+	chain := InitBlockchain(dbTestPath)
+	defer chain.Database.Close()
+
+	iter := chain.Iterator()
+	for {
+		block := iter.Next()
+
+		assert.Equal(t, string(block.Data), "Genesis", "Genesis data doesn't match")
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
 
 }
 
-func TestAddBlock(t *testing.T) {
-	blockchain := InitBlockchain()
-	blockchain.AddBlock("test1")
-	lastBlock := blockchain.Blocks[1]
+func TestInitAddBlock(t *testing.T) {
+	err := os.Mkdir(dbTestPath, 0755)
+	Handle(err)
+	defer os.RemoveAll(dbTestPath)
 
-	assert.Equal(t, string(lastBlock.Data), "test1", "Added block data doesn't match")
+	chain := InitBlockchain(dbTestPath)
+	defer chain.Database.Close()
+
+	chain.AddBlock("test block")
+
+	iter := chain.Iterator()
+	i := 1
+	expectedValues := []string{"Genesis", "test block"}
+
+	for {
+		block := iter.Next()
+
+		assert.Equal(t, string(block.Data), expectedValues[i], fmt.Sprintf("Block %d data doesn't match", i))
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+
+		i--
+	}
 
 }
