2e2529076ae9e51c5c785ed84248dd1c68e274f3
nahumsa <nahumsaa@gmail.com>
2021-09-11 16:51:58 -0300

    add blockchain test

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -9,23 +9,49 @@ import (
 )
 
 const (
-	dbTestPath = "test_blocks"
+	dbTestPath  = "test_blocks"
+	from        = "A"
+	to          = "B"
+	amount      = 5
+	initBalance = 100
 )
 
-func TestInitBlockChain(t *testing.T) {
-
+func setupDB(t *testing.T) func(t *testing.T) {
 	err := os.Mkdir(dbTestPath, 0755)
 	Handle(err)
-	defer os.RemoveAll(dbTestPath)
 
-	chain := InitBlockchain(dbTestPath)
+	return func(t *testing.T) {
+		os.RemoveAll(dbTestPath)
+	}
+}
+
+// TestBlockchain tests initializes, add a block to the blockchain and see if
+// those blocks are valid, check the balance of the sender and the receiver
+func TestBlockchain(t *testing.T) {
+
+	teardownDB := setupDB(t)
+	defer teardownDB(t)
+
+	t.Run("Test the initialization of the blockchain", testInitBlockchain)
+	t.Run("Test the value of the initial adress", testBalanceBeforeTransaction)
+	t.Run("Test adding a block on the blockchain", testAddBlock)
+	t.Run("Test the value of the sender after the transaction", testBalanceSender)
+	t.Run("Test the value of the receiver after the transaction", testBalanceReceiver)
+}
+
+func testInitBlockchain(t *testing.T) {
+
+	chain := InitBlockchain(dbTestPath, from)
 	defer chain.Database.Close()
 
 	iter := chain.Iterator()
+
 	for {
 		block := iter.Next()
 
-		assert.Equal(t, string(block.Data), "Genesis", "Genesis data doesn't match")
+		pow := NewProof(block)
+
+		assert.True(t, pow.Validate(), "Genesis block not valid")
 
 		if len(block.PrevHash) == 0 {
 			break
@@ -34,24 +60,22 @@ func TestInitBlockChain(t *testing.T) {
 
 }
 
-func TestInitAddBlock(t *testing.T) {
-	err := os.Mkdir(dbTestPath, 0755)
-	Handle(err)
-	defer os.RemoveAll(dbTestPath)
+func testAddBlock(t *testing.T) {
 
-	chain := InitBlockchain(dbTestPath)
+	chain := ContinueBlockChain(dbTestPath, from)
 	defer chain.Database.Close()
 
-	chain.AddBlock("test block")
+	tx := NewTransaction(from, to, amount, chain)
+	chain.AddBlock([]*Transaction{tx})
 
 	iter := chain.Iterator()
 	i := 1
-	expectedValues := []string{"Genesis", "test block"}
 
 	for {
 		block := iter.Next()
 
-		assert.Equal(t, string(block.Data), expectedValues[i], fmt.Sprintf("Block %d data doesn't match", i))
+		pow := NewProof(block)
+		assert.True(t, pow.Validate(), fmt.Sprintf("Block %d not valid", i))
 
 		if len(block.PrevHash) == 0 {
 			break
@@ -60,4 +84,37 @@ func TestInitAddBlock(t *testing.T) {
 		i--
 	}
 
+}
+
+func getBalance(address string) int {
+	chain := ContinueBlockChain(dbTestPath, address)
+	defer chain.Database.Close()
+
+	balance := 0
+
+	UTXOs := chain.FindUTXO(address)
+
+	for _, out := range UTXOs {
+		balance += out.Value
+	}
+
+	return balance
+}
+
+func testBalanceBeforeTransaction(t *testing.T) {
+	balanceA := getBalance(from)
+
+	assert.Equal(t, initBalance, balanceA, "Balance for the initial block doesn't match")
+}
+
+func testBalanceSender(t *testing.T) {
+	balanceSender := getBalance(from)
+
+	assert.Equal(t, initBalance-amount, balanceSender, "Balance for the sender doesn't match")
+}
+
+func testBalanceReceiver(t *testing.T) {
+	balanceReceiver := getBalance(to)
+
+	assert.Equal(t, amount, balanceReceiver, "Balance for the receiver doesn't match")
 }
